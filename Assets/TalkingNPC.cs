797433f341b874cd128b57c2b0f5b9a496393e38
Alec Evans <alecevans20118@gmail.com>
2024-03-08 10:57:42 -0800

    toad

--- Assets/TalkingNPC.cs
+++ Assets/TalkingNPC.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TalkingNPC : NPC
 {
+    bool talking = false;
+    public bool wandering;
+    public TextMeshPro text;
+
     // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
         base.Start();
-        SetState(new RoamingNPC_State(new Vector2(1f, 3f)));
+        if (wandering)
+        {
+            SetState(new RoamingNPC_State(new Vector2(1f, 3f)));
+        }
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("player Spotted");
+            if (!talking)
+            {
+                StartCoroutine(Talk());
+            }
+        }
+    }
+
+    public IEnumerator Talk()
+    {
+        talking = true;
+        text.enabled = true;
+        yield return new WaitForSeconds(3f);
+        text.enabled = false;
+        talking = false;
     }
 }
